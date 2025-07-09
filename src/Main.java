f43df4d12194f272648fb77f0dbc518765aa036d
fikalalif <fikalgans26@gmail.com>
2025-07-09 08:55:05 +0700

    j frame form

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-import auth.LoginFrame;
+
 import javax.swing.SwingUtilities;
 /**
  *
@@ -11,7 +11,7 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            new LoginFrame().setVisible(true);
+            new Login().setVisible(true);
         });
     }
 }
